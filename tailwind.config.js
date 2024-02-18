b64bb6f21cdadc2ced0e98911f3d40730e38a0ad
Ngoc Na <buina5555@gmail.com>
2024-02-18 16:47:11 +0700

    responsive-header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     extend: {
       colors: {
         'light-coffee' : '#C89F94'
+      },
+      fontFamily: {
+        Karla: ['Karla', 'sans-serif']
+      },
+      keyframes: {
+        slideDown: {
+          '0%': {transform: 'translateY(-100%)'},
+          '100%': {transform: 'translateY(0)'}
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.4s ease-in-out'
       }
     },
   },
